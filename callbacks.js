a0cc81a3c0df8b1f7770407041bcfbdf29daa797
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-07 10:39:00 -0300

    callbacks

---
+++ callbacks.js
@@ -0,0 +1,22 @@
+const nomes = ['Evaldo', 'Mari', 'Camis'];
+/*
+nomes.forEach(function(nome) {
+    console.log(nome);
+})
+*/
+/*
+nomes.forEach((nome) => { 
+    console.log(nome); 
+});
+*/
+/*
+nomes.forEach((nome) => console.log(nome));
+*/
+/*
+function imprimeNome(nome) {
+    console.log(nome);
+}
+nomes.forEach(imprimeNome);
+*/
+const imprimeNome = (nome) => console.log(nome);
+nomes.forEach(imprimeNome);
