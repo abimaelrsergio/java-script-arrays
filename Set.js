25c61d52ac52ad2dfa45eaa240b71b0536c8f989
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-09 22:35:26 -0300

    Removendo elementos repetidos

---
+++ Set.js
@@ -0,0 +1,9 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João", "Pedro", "Pedro"];
+
+const meuSet = new Set(nomes);
+
+console.log(meuSet);
+
+const nomesAtualizados = [...meuSet];
+
+console.log(nomesAtualizados);
